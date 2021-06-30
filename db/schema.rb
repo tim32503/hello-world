d2feb7a43a52efe082af461093dbd7f965aee817
Tim Hsueh <timhsueh@curihaosity.xyz>
2021-06-30 17:29:51 +0800

    修改: 資料庫改為 pgSQL

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_30_080216) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "content"
